62c031d67383d8aa01cf51346f7f14dbd296bdcf
Daren Serra Dias Warburton <daren.sdw@gmail.com>
2010-04-14 18:05:05 +0100

    order leadership correctly

--- app/controllers/leaderships_controller.rb
+++ app/controllers/leaderships_controller.rb
@@ -3,7 +3,7 @@ class LeadershipsController < ApplicationController
   # GET /leaderships
   # GET /leaderships.xml
   def index
-    @leaderships = Leadership.find(:all, "ORDER BY postion ASC")
+    @leaderships = Leadership.find(:all, :order => "postion ASC")
 
     respond_to do |format|
       format.html # index.html.erb
