f4061adf303f0ab662bc91ab442553f401190b28
Daren Serra Dias Warburton <daren.sdw@gmail.com>
2010-04-14 11:13:10 +0100

    removed paperclip resizing

--- app/models/leadership.rb
+++ app/models/leadership.rb
@@ -1,9 +1,7 @@
-require 'paperclip'
-  
 class Leadership < ActiveRecord::Base
   attr_accessible :postion, :name, :title, :email, :content_1, :content_2, :avatar
   
-  has_attached_file :avatar, :styles => { :small => ["85x85>", :jpg] },
+  has_attached_file :avatar, #:styles => { :small => ["85x85>", :jpg] },
                     :storage => :s3,
                     :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
                     :path => "/leaderships/:attachment/:id/:style/:basename.:extension"
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,9 +1,7 @@
-require 'paperclip'
-
 class Page < ActiveRecord::Base
   attr_accessible :name, :permalink, :content, :photo
   
-  has_attached_file :photo, :styles => { :small => ["200x200>", :jpg], :page => ["580x100", :jpg], :category => ["720x160", :jpg]  },
+  has_attached_file :photo, #:styles => { :small => ["200x200>", :jpg], :page => ["580x100", :jpg], :category => ["720x160", :jpg]  },
                     :storage => :s3,
                     :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
                     :path => "/pages/:attachment/:id/:style/:basename.:extension"
