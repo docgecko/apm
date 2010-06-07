1235754a56dcd08cd1c584a66761dfa70503d62e
Daren Serra Dias Warburton <daren.sdw@gmail.com>
2010-06-07 17:18:00 +0100

    converted erb to haml, each page still needs reviewing

--- config/environment.rb
+++ config/environment.rb
@@ -25,6 +25,7 @@ Rails::Initializer.run do |config|
   config.gem "authlogic", :version => "2.1.3"
   config.gem "heroku"
   config.gem "memcached", :version => ">= 0.19.3"
+  config.gem "haml", :version => ">= 3.0.10"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
