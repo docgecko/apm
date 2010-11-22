fd715b9516bf0aacfc8e11da3e38fb51e7763db6
Daren Serra Dias Warburton <daren.sdw@gmail.com>
2010-11-22 19:19:55 +0000

    notifiers etc

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -12,10 +12,10 @@ class RegistrationsController < InheritedResources::Base
   def create
     if Notifier.registration(params[:registration]).deliver
       flash[:notice] = "Your request for a Class was successfully sent to apm."
-      redirect_to(training_event_registration_path)
+      redirect_to(training_event_registration_index_path)
     else
       flash.now[:error] = "An error occurred while sending this request. Instead, please click <a href='&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#99;&#111;&#110;&#116;&#97;&#99;&#116;&#64;&#97;&#112;&#109;&#46;&#101;&#117;&#46;&#99;&#111;&#109;'>here</a> to contact us."
-      redirect_to(training_event_registration_path)
+      redirect_to(training_event_registration_index_path)
     end
   end
 end
--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -11,7 +11,7 @@ class Notifier < ActionMailer::Base
     @message = sender[:message]
 
     mail(:from => sender[:email],
-         :subject => "[APM Website] #{sender[:subject]}")
+         :subject => "[APM Website] Contact Message")
   end
   
   def onsite(sender)
@@ -31,7 +31,7 @@ class Notifier < ActionMailer::Base
     @objectives = sender[:objectives]
 
     mail(:from => sender[:email],
-         :subject => "[APM Website] #{sender[:subject]}")
+         :subject => "[APM Website] Request for Quote for Class in #{sender[:course]}")
   end
   
   def request_class(sender)
@@ -46,6 +46,30 @@ class Notifier < ActionMailer::Base
     @when = sender[:when]
 
     mail(:from => sender[:email],
-         :subject => "[APM Website] #{sender[:subject]}")
+         :subject => "[APM Website] Request a Class in #{sender[:course]}")
+  end
+
+  def registration(sender)
+    @name = sender[:name]
+    @organisation = sender[:organisation]
+    @role = sender[:role]
+    @email = sender[:email]
+    @telephone = sender[:telephone]
+    @mobile = sender[:mobile]
+    @address_type = sender[:address_type]
+    @address1 = sender[:address1]
+    @address2 = sender[:address2]
+    @city = sender[:city]
+    @county = sender[:county]
+    @postcode = sender[:postcode]
+    @country = sender[:country]
+    @course = sender[:course]
+    @course_id = sender[:course_id]
+    @schedule_start = sender[:schedule_start]
+    @schedule_end = sender[:schedule_end]
+    @referral = sender[:referral]
+
+    mail(:from => sender[:email],
+         :subject => "[APM Website] Register for a Class in #{sender[:course]}")
   end
 end
